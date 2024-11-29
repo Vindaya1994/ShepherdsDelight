c065c9ac324ec8d99f83bda83a93e830eda7e1ae
Roshelvimukthi <roshelfernando.v@gmail.com>
2024-11-29 21:20:33 +0530

    opening page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { ViewportScroller, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ShepherdsDelights';
+  contentLoaded = false;  // Flag to control content loading
+
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  // Method to load content when button is clicked
+  loadContent(): void {
+    this.contentLoaded = true;
+  }
+
+  // Scroll to the specified section
+  scrollToSection(sectionId: string): void {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
 }
